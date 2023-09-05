b5f1c64c94232dfb7d2a236405870571cc635bee
aryark <aryark3112@gmail.com>
2023-09-05 12:00:01 +0530

    first commit

---
+++ razor View Engine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace razor_View_Engine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
